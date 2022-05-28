f1aafe077c96ab2bb916d2c22845bbb45c4d48e6
(yunjin) <sion11290805@gmail.com>
2022-05-28 18:23:13 +0900

    feat: card Context 추가

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -2,4 +2,5 @@ export { default as PayPage } from './PayPage';
 export { default as AddCardPAge } from './AddCardPage';
 export { default as CardEditPage } from './CardEditPage';
 export { default as PaymentPage } from './PaymentPage';
-export { default as PayProvider } from '../context/PageProvider';
+export { default as KyoulPayPageProvider } from '../context/PageProvider';
+export { default as KyoulPayProvider  } from '../context/Provider';
