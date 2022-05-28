8a10e5a0ad62b824b7eef1f84824300d82cf6919
(yunjin) <sion11290805@gmail.com>
2022-05-28 16:03:09 +0900

    feat: npm 배포 page Context 추가

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -2,3 +2,4 @@ export { default as PayPage } from './PayPage';
 export { default as AddCardPAge } from './AddCardPage';
 export { default as CardEditPage } from './CardEditPage';
 export { default as PaymentPage } from './PaymentPage';
+export { default as PayProvider } from '../context/PageProvider';
